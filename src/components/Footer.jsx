d1b1bc342a01ab070fc272c8b4898e4c6fefc336
dianannperla <waafirmelyza@hotmail.com>
2025-09-07 20:29:13 +0100

    feat: initial commit for src/components/Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Bản quyền © 2023 Quang Frontend Templates</p>
+    </footer>
+  );
+};
+
+export default Footer;
