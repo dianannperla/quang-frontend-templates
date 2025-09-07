a06f3644a3519be319b2900098276d323f45a921
dianannperla <waafirmelyza@hotmail.com>
2025-09-07 20:35:35 +0100

    Update greeting message for better clarity.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Entry point for the project
 console.log('Welcome to Quang Frontend Templates!');
-console.log('Wishing you a fantastic coding experience, dear developer!');
+console.log('Wishing you an amazing coding experience, dear developer!');
 console.log('Happy coding! 😊');
